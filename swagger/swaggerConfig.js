b86aa509d4d373bb5666ddac5df82bd1d7dccf39
Ahmed Sabry <ahmed010144497@gmail.com>
2024-09-09 15:56:19 +0300

    add .env

--- swagger/swaggerConfig.js
+++ swagger/swaggerConfig.js
@@ -20,7 +20,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3000"
+                url: "https://youtubeplatformapi-production.up.railway.app"
             }
         ],
         security: [
